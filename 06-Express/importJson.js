9e13ddaf221c2f6d650bd7953ac8799c5fcdb669
Johnny Ramos <jrbramos05@gmail.com>
2026-02-04 22:01:47 -0300

    add pagination logic

--- 06-Express/importJson.js
+++ 06-Express/importJson.js
@@ -9,7 +9,7 @@ const app = express();
 app.get('/products', (req, res) => {
     // Express has an incredible way to get the query params with any effort. In express query params are stored in the request object.
     // So we can destructuring 'req.query' to obtain every query and manipulate it.
-    const { text, category, hasDelivery, limit, offset } = req.query;
+    const { text, category, hasDelivery } = req.query;
     // When we have different filters to apply, we can avoid writing repetitive code by grouping them together.
     // The following block of code shows only one filter with three different conditions.
     let filteredProducts = products.filter(product => {
@@ -34,7 +34,14 @@ app.get('/products', (req, res) => {
         }
         return true;
     });
-    return res.json(filteredProducts);
+    // Pagination
+    // Remember convert the querys in number because Express give you all querys as a string and we need numbers for math.
+    const limit = Number(req.query.limit) || 5;
+    const offset = Number(req.query.offset) || 0;
+    // Get a slice of our array using offset and limit as parameters.
+    const paginatedProducts = filteredProducts.slice(offset, offset + limit);
+
+    return res.json(paginatedProducts);
 });
 // Additionally, another method to import data exists. In this case, we are importing dynamically the information.
 // The difference is that in the first case the json is imported when the web is loaded. And in the second case the information is loaded
